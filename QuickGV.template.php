bddd08fd0bd8751aebf9230b947b52528583e284
Raymond Wu (GitHub) <virus.warnning@gmail.com>
2014-06-19 14:26:04 +0800

    fixed #16, add 4 themes * warm * cold * sakura * default - like default MediaWiki style

--- QuickGV.template.php
+++ QuickGV.template.php
@@ -3,6 +3,42 @@
  * Template of graph description (dot syntax)
  *************************************************/
 
+// theme definitions
+$THEME_ATTRS = array(
+	'cold' => array(
+		'graph_background' => '#000066',
+		'edge_path'   => '#ffffff',
+		'edge_font'   => '#ffffff',
+		'node_border' => '#ffffff',
+		'node_font'   => '#000000',
+		'node_fill'   => '#ccffff:#00c0ff'
+	),
+	'warm' => array(
+		'graph_background' => '#fffff7',
+		'edge_path'   => '#704000',
+		'edge_font'   => '#704000',
+		'node_border' => '#c07000',
+		'node_font'   => '#000000',
+		'node_fill'   => '#ffffff:#ffffc0'
+	),
+	'sakura' => array(
+		'graph_background' => '#996677',
+		'edge_path'   => '#ffffff',
+		'edge_font'   => '#ffffff',
+		'node_border' => '#cc4444',
+		'node_font'   => '#000000',
+		'node_fill'   => '#ffffff:#ffc0d0'
+	),
+	'default' => array(
+		'graph_background' => '#f0f0f0:#ffffff',
+		'edge_path'   => '#555555',
+		'edge_font'   => '#000000',
+		'node_border' => '#aaaaaa',
+		'node_font'   => '#000000',
+		'node_fill'   => '#ffffff:#e7e7e7'
+	),
+);
+
 /**
  * Get shell argument. If not exists, use default instead.
  *
@@ -31,22 +67,8 @@ if ($usage=='mindmap') $usage = 'neato';
 // get graph description
 $gdata = trim(file_get_contents('php://stdin'));
 
-// theme definitions
-// TODO: move theme attributes to a separate PHP file
-$theme_attrs = array(
-	'blue' => array(
-		'node_border' => '#0070c0',
-		'node_font'   => '#000000',
-		'node_fill'   => '#ffffff:#00c0ff'
-	),
-	'default' => array(
-		'node_border' => '#c07000',
-		'node_font'   => '#000000',
-		'node_fill'   => '#ffffff:#ffffc0'
-	),
-);
-
-if (!isset($theme_attrs[$theme])) $theme = 'default';
+if (!isset($THEME_ATTRS[$theme])) $theme = 'default';
+$attrs =& $THEME_ATTRS[$theme];
 ?>
 digraph <?php echo $gname; ?> {
 
@@ -57,8 +79,11 @@ digraph <?php echo $gname; ?> {
 	// default settings of graphs
 	graph [
 		rankdir = LR,
+		bgcolor = "<?php echo $attrs['graph_background']; ?>",
+		gradientangle = 65,
 		<?php if ($usage=='neato'): ?>
-		layout = neato;
+		layout = neato,
+		start  = "A",
 		<?php endif; ?>
 	];
 
@@ -76,16 +101,16 @@ digraph <?php echo $gname; ?> {
 		fontsize = 10,
 		
 		// theme
-		color     = "<?php echo $theme_attrs[$theme]['node_border']; ?>",
-		fontcolor = "<?php echo $theme_attrs[$theme]['node_font']; ?>",
-		fillcolor = "<?php echo $theme_attrs[$theme]['node_fill']; ?>",
-		gradientangle = 285
+		color     = "<?php echo $attrs['node_border']; ?>",
+		fontcolor = "<?php echo $attrs['node_font']; ?>",
+		fillcolor = "<?php echo $attrs['node_fill']; ?>",
+		gradientangle = 295 // left, top -> right, bottom
 	];
 
 	// default settings of edges
 	edge [
-		color     = "#444444", // line
-		fontcolor = "#444444", // text
+		color     = "<?php echo $attrs['edge_path']; ?>",
+		fontcolor = "<?php echo $attrs['edge_font']; ?>",
 		fontsize  = 10
 	];
 
